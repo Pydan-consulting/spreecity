fb82a11e0ff94a7bdd612822bdde43ccb272595f
ganesh <ganesh.pydan@gmail.com>
2012-01-11 20:33:35 +0530

    deleted spreecity project

--- spreecity/user_management/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('spreecity.user_management.views',
-    (r'^member_creation/', 'member_creation'),  
-    (r'^partner_creation/', 'partner_creation'), 
- 
-)
